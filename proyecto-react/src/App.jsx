76ced2440fcc55e568ee9b110a6d3985af662b1e
Alejandro Ortiz Quiroz <alejandro.ortizq1@udea.edu.co>
2022-01-07 21:27:05 -0500

    Avance Clase 7 min 30:50, tailwind incorporado

---
+++ proyecto-react/src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './styles/styles.css';
+
+function App() {
+  return (
+    <div className='fondoRojo'>
+      Hola Perritas
+    </div>
+  );
+}
+
+export default App;
