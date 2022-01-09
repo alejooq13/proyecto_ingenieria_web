7aeae62b15a219d6213c5bb2e8b3130db2ef6516
Alejandro Ortiz Quiroz <alejandro.ortizq1@udea.edu.co>
2022-01-09 01:48:12 -0500

    C7 min 52, correcciones sidebar, revisar mejor forma de ajustar items alineados

--- proyecto-react/src/components/sidebarPerfil.jsx
+++ proyecto-react/src/components/sidebarPerfil.jsx
@@ -5,9 +5,9 @@ function Sidebar() {
             {/* Sidebar starts */}
             {/* Remove class [ hidden ] and replace [ sm:flex ] with [ flex ] */}
             <div className="w-64 absolute sm:relative bg-slate-900 shadow lg:h-full flex-col justify-between hidden sm:flex">
-                <div>
+                <div className="py-3">
                     <div className="flex items-center px-8 py-4">
-                        <img className="rounded-full" src="https://media-exp1.licdn.com/dms/image/C5603AQEUYbA5Fo-blQ/profile-displayphoto-shrink_800_800/0/1618197473111?e=1647475200&v=beta&t=cleKA-fHE_dOYHgGi98bZh51fZvb8QvksEB5spKPbiU" width={200} height={200}/>
+                        <img className="rounded-full" src="https://media-exp1.licdn.com/dms/image/C5603AQFZhNAcKbZI8g/profile-displayphoto-shrink_800_800/0/1641708296490?e=1647475200&v=beta&t=cdNCDScUyktW7Nf3kpPz3KjPrHj-ZBEVYqw1cim2Ju4" width={200} height={200}/>
                     </div>
                     <div className="flex flex-col items-center text-gray-300 font-bold">
                         <span>Alejandro Ortiz Quiroz</span>
@@ -15,7 +15,7 @@ function Sidebar() {
                         <span>Ingeniero de Sistemas</span>
                     </div>
                     <ul className="mt-12">
-                        
+                        <div /*Se recorto parte de cod 1*/ ></div>
                         <li className="flex w-full justify-between text-gray-300 hover:text-white hover:bg-sky-600 cursor-pointer items-center px-8 py-3">
                             <div className="flex items-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-compass" width={18} height={18} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
@@ -69,121 +69,11 @@ function Sidebar() {
                         </li>
                     </ul>
                 </div>
-                <div className="px-8 bg-slate-800">
-                    <ul className="w-full flex items-center justify-between ">
-                        <li className="cursor-pointer text-white pt-5 pb-3">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-bell" width={20} height={20} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" />
-                                <path d="M10 5a2 2 0 0 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6" />
-                                <path d="M9 17v1a3 3 0 0 0 6 0v-1" />
-                            </svg>
-                        </li>
-                        <li className="cursor-pointer text-white pt-5 pb-3">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-messages" width={20} height={20} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" />
-                                <path d="M21 14l-3 -3h-7a1 1 0 0 1 -1 -1v-6a1 1 0 0 1 1 -1h9a1 1 0 0 1 1 1v10" />
-                                <path d="M14 15v2a1 1 0 0 1 -1 1h-7l-3 3v-10a1 1 0 0 1 1 -1h2" />
-                            </svg>
-                        </li>
-                        <li className="cursor-pointer text-white pt-5 pb-3">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-settings" width={20} height={20} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" />
-                                <path d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
-                                <circle cx={12} cy={12} r={3} />
-                            </svg>
-                        </li>
-                        <li className="cursor-pointer text-white pt-5 pb-3">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-archive" width={20} height={20} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" />
-                                <rect x={3} y={4} width={18} height={4} rx={2} />
-                                <path d="M5 8v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-10" />
-                                <line x1={10} y1={12} x2={14} y2={12} />
-                            </svg>
-                        </li>
-                    </ul>
+                <div className="px-8 bg-slate-800 flex items-center justify-center text-xs h-10 text-white">
+                    Medellín - Colombia 
                 </div>
             </div>
-            <div className="text-gray-600 mr-8 visible sm:hidden relative" onclick="dropdownHandler(this)">
-                <ul className="p-y w-64 border-r bg-indigo-900 absolute rounded left-0 shadow mt-8 sm:mt-16 hidden">
-                    <li className="flex w-full justify-between text-gray-600 hover:text-gray-300 hover:bg-indigo-800 cursor-pointer items-center py-15 px-2">
-                        <div className="flex items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-grid" width={18} height={18} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" />
-                                <rect x={4} y={4} width={6} height={6} rx={1} />
-                                <rect x={14} y={4} width={6} height={6} rx={1} />
-                                <rect x={4} y={14} width={6} height={6} rx={1} />
-                                <rect x={14} y={14} width={6} height={6} rx={1} />
-                            </svg>
-                            <span className="text-sm  ml-2">Dashboard</span>
-                        </div>
-                    </li>
-                    <li className="flex w-full justify-between text-gray-600 hover:text-gray-300 hover:bg-indigo-800 cursor-pointer items-center px-2 py-3">
-                        <div className="flex items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-puzzle" width={18} height={18} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" />
-                                <path d="M4 7h3a1 1 0 0 0 1 -1v-1a2 2 0 0 1 4 0v1a1 1 0 0 0 1 1h3a1 1 0 0 1 1 1v3a1 1 0 0 0 1 1h1a2 2 0 0 1 0 4h-1a1 1 0 0 0 -1 1v3a1 1 0 0 1 -1 1h-3a1 1 0 0 1 -1 -1v-1a2 2 0 0 0 -4 0v1a1 1 0 0 1 -1 1h-3a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1h1a2 2 0 0 0 0 -4h-1a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1" />
-                            </svg>
-                            <span className="text-sm  ml-2">Products</span>
-                        </div>
-                    </li>
-                    <li className="flex w-full justify-between text-gray-600 hover:text-gray-300 hover:bg-indigo-800 cursor-pointer items-center px-2 py-3">
-                        <div className="flex items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-compass" width={18} height={18} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" />
-                                <polyline points="8 16 10 10 16 8 14 14 8 16" />
-                                <circle cx={12} cy={12} r={9} />
-                            </svg>
-                            <span className="text-sm  ml-2">Performance</span>
-                        </div>
-                    </li>
-                    <li className="flex w-full justify-between text-gray-600 hover:text-gray-300 hover:bg-indigo-800 cursor-pointer items-center px-2 py-3">
-                        <div className="flex items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-code" width={20} height={20} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" />
-                                <polyline points="7 8 3 12 7 16" />
-                                <polyline points="17 8 21 12 17 16" />
-                                <line x1={14} y1={4} x2={10} y2={20} />
-                            </svg>
-                            <span className="text-sm  ml-2">Deliverables</span>
-                        </div>
-                    </li>
-                    <li className="flex w-full justify-between text-gray-600 hover:text-gray-300 hover:bg-indigo-800 cursor-pointer items-center  px-2 py-3">
-                        <div className="flex items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-puzzle" width={18} height={18} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" />
-                                <path d="M4 7h3a1 1 0 0 0 1 -1v-1a2 2 0 0 1 4 0v1a1 1 0 0 0 1 1h3a1 1 0 0 1 1 1v3a1 1 0 0 0 1 1h1a2 2 0 0 1 0 4h-1a1 1 0 0 0 -1 1v3a1 1 0 0 1 -1 1h-3a1 1 0 0 1 -1 -1v-1a2 2 0 0 0 -4 0v1a1 1 0 0 1 -1 1h-3a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1h1a2 2 0 0 0 0 -4h-1a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1" />
-                            </svg>
-                            <span className="text-sm  ml-2">Invoices</span>
-                        </div>
-                    </li>
-                    <li className="flex w-full justify-between text-gray-600 hover:text-gray-300 hover:bg-indigo-800 cursor-pointer items-center px-2 py-3">
-                        <div className="flex items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-stack" width={18} height={18} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" />
-                                <polyline points="12 4 4 8 12 12 20 8 12 4" />
-                                <polyline points="4 12 12 16 20 12" />
-                                <polyline points="4 16 12 20 20 16" />
-                            </svg>
-                            <span className="text-sm  ml-2">Inventory</span>
-                        </div>
-                    </li>
-                    <li className="flex w-full justify-between text-gray-600 hover:text-gray-300 hover:bg-indigo-800 cursor-pointer items-center px-2 py-3">
-                        <div className="flex items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-settings" width={18} height={18} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" />
-                                <path d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
-                                <circle cx={12} cy={12} r={3} />
-                            </svg>
-                            <span className="text-sm  ml-2">Settings</span>
-                        </div>
-                    </li>
-                </ul>
-                <svg aria-label="Main Menu" aria-haspopup="true" xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-menu cursor-pointer" width={30} height={30} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" />
-                    <line x1={4} y1={8} x2={20} y2={8} />
-                    <line x1={4} y1={16} x2={20} y2={16} />
-                </svg>
-            </div>
+            <div /*Parte 3*/></div>
         </div>
     );
 }
--- proyecto-react/src/pages/index.jsx
+++ proyecto-react/src/pages/index.jsx
@@ -6,7 +6,15 @@ const Index = ()=>{
     return(
         <div className='h-screen flex'>
             <Sidebar/>
-            <span>Hola</span>
+            <div className='flex justify-center py-5'>
+            <div className='flex flex-col items-center justify-center bg-slate-200 rounded-lg w-8/12 h-1/5'>
+                <span className='py-2'>Sobre mi</span>
+                <span className='parrafo w-4/5 text-justify'>
+                    Tengo grandes capacidades lógicas, matemáticas y analíticas, las cuales, combinadas a mi conocimiento en economía, finanzas, mercadeo, ciencia de datos y programación, me permiten comprender y modelar procesos técnicos, empresariales y logísticos para ser más asertivo en él análisis de opciones y toma de decisiones
+                </span>
+            </div>
+            </div>
+            
         </div>
     )
 }
